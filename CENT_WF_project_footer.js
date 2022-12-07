f2ac06cf74a836871dd99f6c98e84d1a8fe1aed6
Jorge Verdera <57467445+jverdera19@users.noreply.github.com>
2022-12-07 04:48:08 -0800

    deadline script

--- CENT_WF_project_footer.js
+++ CENT_WF_project_footer.js
@@ -790,6 +790,7 @@ function removeItemsNotAvailableReviewOrder() {
     addLoadingIcon(reviewItemContainer)
 }
 
+// Jetboost filters
 if (
     !window.location.pathname.match(/all-vendors/) &&
     !window.location.pathname.match(/find/) &&
@@ -999,4 +1000,53 @@ function checkDeliveryType() {
     } else {
         document.querySelector('.shipping-disclaimer').style.display = 'none'
     }
+}
+
+function setDeadline(deadlineDate) {
+    
+    var deadline = deadlineDate // format - '2022/12/21 23:59';
+
+    function pad(num, size) {
+        var s = "0" + num;
+        return s.substr(s.length - size);
+    }
+
+    // fixes "Date.parse(date)" on safari
+    function parseDate(date) {
+        const parsed = Date.parse(date);
+        if (!isNaN(parsed)) return parsed
+        return Date.parse(date.replace(/-/g, '/').replace(/[a-z]+/gi, ' '));
+    }
+
+    function getTimeRemaining(endtime) {
+        let total = parseDate(endtime) - Date.parse(new Date())
+        let seconds = Math.floor((total / 1000) % 60)
+        let minutes = Math.floor((total / 1000 / 60) % 60)
+        let hours = Math.floor((total / (1000 * 60 * 60)) % 24)
+        let days = Math.floor(total / (1000 * 60 * 60 * 24))
+
+        return { total, days, hours, minutes, seconds };
+    }
+
+    function clock(id, endtime) {
+        let days = document.getElementById(id + '-days')
+        let hours = document.getElementById(id + '-hours')
+        let minutes = document.getElementById(id + '-minutes')
+        let seconds = document.getElementById(id + '-seconds')
+
+        var timeinterval = setInterval(function () {
+            var time = getTimeRemaining(endtime);
+
+            if (time.total <= 0) {
+                clearInterval(timeinterval);
+            } else {
+                days.innerHTML = pad(time.days, 2);
+                hours.innerHTML = pad(time.hours, 2);
+                minutes.innerHTML = pad(time.minutes, 2);
+                seconds.innerHTML = pad(time.seconds, 2);
+            }
+        }, 1000);
+    }
+
+    clock('js-clock', deadline);
 }
