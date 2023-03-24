46a0fd8b1490e6a8a46e354544f956c788931564
Jorge Verdera <57467445+jverdera19@users.noreply.github.com>
2023-03-24 07:55:47 -0700

    Updating to use baDeliveryDate

--- CENT_WF_project_footer.js
+++ CENT_WF_project_footer.js
@@ -207,7 +207,7 @@ function checkDateProject() {
             }
             
             // MARK: Make this pick first Saturday dynamicly
-            iSODate = new Date("Mar 25, 2023");
+            iSODate = new Date(baDeliveryDate);
             console.warn('testing new date', baDeliveryDate);
         } else if (window.location.pathname.startsWith('/product/') || window.location.pathname.startsWith('/vendor/')) {
             let hiddenDatesList = hiddenDatesContainer.querySelectorAll('.hidden-date');
