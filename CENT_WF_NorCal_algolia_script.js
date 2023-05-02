48a05d7bc70741c76b5b3ede1691560ba240bfa1
Jorge Verdera <110677688+jorgelocale@users.noreply.github.com>
2023-05-02 06:10:46 -0700

    Disabled enter key on search bar

--- CENT_WF_NorCal_algolia_script.js
+++ CENT_WF_NorCal_algolia_script.js
@@ -124,68 +124,94 @@ const { setIsOpen } = autocomplete({
                                     'a86f4e69920bf_dashboard_generated_id',
                             },
                             {
-                                "vendor_name": "b. patisserie",
-                                "vendor_slug": "ba-b-patisserie",
-                                "vendor_logo_url": "https://uploads-ssl.webflow.com/6226a2abeb906f60cc1f024c/6414bb268bd00d0a24339ffd_Kk96S8TwFpSYXtlDZbv02TvxHhTLOWRF7J3Q036voV8.png",
-                                "vendor_products_array": "[\"Granola\", \"Signature Box\", \"Classic Kouign Amann Box\", \"Mixed Kouign Amann Box\"]",
-                                "objectID": "1517be13837576_dashboard_generated_id"
+                                vendor_name: 'b. patisserie',
+                                vendor_slug: 'ba-b-patisserie',
+                                vendor_logo_url:
+                                    'https://uploads-ssl.webflow.com/6226a2abeb906f60cc1f024c/6414bb268bd00d0a24339ffd_Kk96S8TwFpSYXtlDZbv02TvxHhTLOWRF7J3Q036voV8.png',
+                                vendor_products_array:
+                                    '["Granola", "Signature Box", "Classic Kouign Amann Box", "Mixed Kouign Amann Box"]',
+                                objectID:
+                                    '1517be13837576_dashboard_generated_id',
                             },
                             {
-                                "vendor_name": "Pizzeria Delfina",
-                                "vendor_slug": "ba-pizzeria-delfina",
-                                "vendor_logo_url": "https://uploads-ssl.webflow.com/6226a2abeb906f60cc1f024c/640a59278a6c881512c56037_y3oQ8-RUP-xHLGIPgY6KKZFJjkpPRF0XqYJ-rT2zZu0.png",
-                                "vendor_products_array": "[\"Latkes with Pear and Quince Conserva\", \"Pomodoro Sauce\", \"Broccoli Rabe Pizza\", \"Margherita Pizza\", \"4 Formaggi Pizza\", \"Funghi Pizza\", \"Napoletana Pizza\", \"Pear and Quince Conserva\", \"Famous Delfina Potato Latkes\", \"Gemini Pizza\", \"Insalata Tricolore Salad\", \"Delfina's Potato Gnocchi\"]",
-                                "objectID": "1090ade1957f1_dashboard_generated_id"
+                                vendor_name: 'Pizzeria Delfina',
+                                vendor_slug: 'ba-pizzeria-delfina',
+                                vendor_logo_url:
+                                    'https://uploads-ssl.webflow.com/6226a2abeb906f60cc1f024c/640a59278a6c881512c56037_y3oQ8-RUP-xHLGIPgY6KKZFJjkpPRF0XqYJ-rT2zZu0.png',
+                                vendor_products_array:
+                                    '["Latkes with Pear and Quince Conserva", "Pomodoro Sauce", "Broccoli Rabe Pizza", "Margherita Pizza", "4 Formaggi Pizza", "Funghi Pizza", "Napoletana Pizza", "Pear and Quince Conserva", "Famous Delfina Potato Latkes", "Gemini Pizza", "Insalata Tricolore Salad", "Delfina\'s Potato Gnocchi"]',
+                                objectID:
+                                    '1090ade1957f1_dashboard_generated_id',
                             },
                             {
-                                "vendor_name": "Monterey Fish Market",
-                                "vendor_slug": "ba-monterey-fish-market",
-                                "vendor_logo_url": "https://uploads-ssl.webflow.com/6226a2abeb906f60cc1f024c/640a593ecda49ce51b7f90b7_JWZkVCqIbgK-JcDfL5ktrUIRWsejOcGkwQpBjFc66Js.jpeg",
-                                "vendor_products_array": "[\"Bodega Rockfish\", \"White Sturgeon Caviar\", \"Swordfish Steaks\", \"Salmon Burger Patty\", \"Parsley Garlic Marinated Gulf Shrimp\", \"New Bedford Scallops\", \"Lobster Tails from Maine\", \"Ft Bragg Petrare Sole\", \"Fort Bragg Picked Crab\", \"Wild Alaskan Halibut\", \"Crab Cakes\", \"Half Moon Bay Black Cod\", \"Catfish\", \"British Columbia King Farmed Salmon\", \"Wild Alaskan True Cod\", \"Lemon Ginger Halibut with Brussels Sprouts Meal Kit for Two\", \"Lemon Ginger Halibut with Brussels Sprouts Meal Kit for One\", \"Farmed Trout\", \"Cooked Octopus Tentacles\", \"Balmoral Smoked Salmon\", \"Local Halibut Fillet\", \"Cocktail Sauce\", \"Canadian Atlantic Farmed Salmon\", \"Branzino\"]",
-                                "objectID": "ab0d7a7b1799_dashboard_generated_id"
+                                vendor_name: 'Monterey Fish Market',
+                                vendor_slug: 'ba-monterey-fish-market',
+                                vendor_logo_url:
+                                    'https://uploads-ssl.webflow.com/6226a2abeb906f60cc1f024c/640a593ecda49ce51b7f90b7_JWZkVCqIbgK-JcDfL5ktrUIRWsejOcGkwQpBjFc66Js.jpeg',
+                                vendor_products_array:
+                                    '["Bodega Rockfish", "White Sturgeon Caviar", "Swordfish Steaks", "Salmon Burger Patty", "Parsley Garlic Marinated Gulf Shrimp", "New Bedford Scallops", "Lobster Tails from Maine", "Ft Bragg Petrare Sole", "Fort Bragg Picked Crab", "Wild Alaskan Halibut", "Crab Cakes", "Half Moon Bay Black Cod", "Catfish", "British Columbia King Farmed Salmon", "Wild Alaskan True Cod", "Lemon Ginger Halibut with Brussels Sprouts Meal Kit for Two", "Lemon Ginger Halibut with Brussels Sprouts Meal Kit for One", "Farmed Trout", "Cooked Octopus Tentacles", "Balmoral Smoked Salmon", "Local Halibut Fillet", "Cocktail Sauce", "Canadian Atlantic Farmed Salmon", "Branzino"]',
+                                objectID: 'ab0d7a7b1799_dashboard_generated_id',
                             },
                             {
-                                "vendor_name": "Pop Pie Co",
-                                "vendor_slug": "ba-pop-pie-co",
-                                "vendor_logo_url": "https://uploads-ssl.webflow.com/6226a2abeb906f60cc1f024c/640a5922e6a5017c498c1ee1_X2nN8AAFAUZmh75WvEf9cBOMyrZUP0gXnR3fkvE2sZI.png",
-                                "vendor_products_array": "[\"Classic Chicken Pot Pie\", \"Steak and Ale Pot Pie\", \"Roasted Veggies & Yellow Curry Pie\", \"Mushroom & Ricotta Pizza Pot Pie\", \"Traditional Aussie Pie\"]",
-                                "objectID": "a4b4b1554894b_dashboard_generated_id"
+                                vendor_name: 'Pop Pie Co',
+                                vendor_slug: 'ba-pop-pie-co',
+                                vendor_logo_url:
+                                    'https://uploads-ssl.webflow.com/6226a2abeb906f60cc1f024c/640a5922e6a5017c498c1ee1_X2nN8AAFAUZmh75WvEf9cBOMyrZUP0gXnR3fkvE2sZI.png',
+                                vendor_products_array:
+                                    '["Classic Chicken Pot Pie", "Steak and Ale Pot Pie", "Roasted Veggies & Yellow Curry Pie", "Mushroom & Ricotta Pizza Pot Pie", "Traditional Aussie Pie"]',
+                                objectID:
+                                    'a4b4b1554894b_dashboard_generated_id',
                             },
                             {
-                                "vendor_name": "Gjusta",
-                                "vendor_slug": "ba-gjusta",
-                                "vendor_logo_url": "https://uploads-ssl.webflow.com/6226a2abeb906f60cc1f024c/641e2b4fe1991c3b653705b1_4MYSCu2mF3_yvfvCOi5sh5r7zGiuWlstR1tBmkdg7nU.png",
-                                "vendor_products_array": "[\"Eggplant Bake\", \"Vegetarian Lasagna\", \"Olive Oil Hummus\", \"Meatballs\", \"Honey Butter\", \"Kimchi\", \"Marinated Grilled Peppers\", \"Marinated Mixed Olives\", \"Beef Lasagna\", \"Whipped Goat Cheese & Walnuts\", \"Minestrone Soup\", \"Baba Ganoush\", \"Chicken Dumpling Soup\", \"Tuna Conserva Salad\"]",
-                                "objectID": "e015690f33079_dashboard_generated_id"
+                                vendor_name: 'Gjusta',
+                                vendor_slug: 'ba-gjusta',
+                                vendor_logo_url:
+                                    'https://uploads-ssl.webflow.com/6226a2abeb906f60cc1f024c/641e2b4fe1991c3b653705b1_4MYSCu2mF3_yvfvCOi5sh5r7zGiuWlstR1tBmkdg7nU.png',
+                                vendor_products_array:
+                                    '["Eggplant Bake", "Vegetarian Lasagna", "Olive Oil Hummus", "Meatballs", "Honey Butter", "Kimchi", "Marinated Grilled Peppers", "Marinated Mixed Olives", "Beef Lasagna", "Whipped Goat Cheese & Walnuts", "Minestrone Soup", "Baba Ganoush", "Chicken Dumpling Soup", "Tuna Conserva Salad"]',
+                                objectID:
+                                    'e015690f33079_dashboard_generated_id',
                             },
                             {
-                                "vendor_name": "Lord Empanada",
-                                "vendor_slug": "ba-lord-empanada",
-                                "vendor_logo_url": "https://uploads-ssl.webflow.com/6226a2abeb906f60cc1f024c/6414966cc92f0cc9a3108000_qTqE-gDAPjY_OgBinpF4GjjEtd3l_Uf4cGDnKcQ2d4I.png",
-                                "vendor_products_array": "[\"Beef Empanadas\", \"Spinach & Cheese Empanadas (Vegetarian)\", \"Chicken Empanadas\", \"Spicy Black Bean Empanadas (Vegan)\"]",
-                                "objectID": "1091c723b3561c_dashboard_generated_id"
+                                vendor_name: 'Lord Empanada',
+                                vendor_slug: 'ba-lord-empanada',
+                                vendor_logo_url:
+                                    'https://uploads-ssl.webflow.com/6226a2abeb906f60cc1f024c/6414966cc92f0cc9a3108000_qTqE-gDAPjY_OgBinpF4GjjEtd3l_Uf4cGDnKcQ2d4I.png',
+                                vendor_products_array:
+                                    '["Beef Empanadas", "Spinach & Cheese Empanadas (Vegetarian)", "Chicken Empanadas", "Spicy Black Bean Empanadas (Vegan)"]',
+                                objectID:
+                                    '1091c723b3561c_dashboard_generated_id',
                             },
                             {
-                                "vendor_name": "Manresa Bread",
-                                "vendor_slug": "ba-manresa-bread",
-                                "vendor_logo_url": "https://uploads-ssl.webflow.com/6226a2abeb906f60cc1f024c/640a594afcee39868b7e1cbf_7fm8EcZgjt4oWPr07HKNqtLX88X-NmQwUYv2InnwogA.png",
-                                "vendor_products_array": "[\"Gluten Free Poppy Seed Raspberry Muffin\", \"Sourdough Bread\", \"Manresa Almond Croissant\", \"Monkey Bread\", \"Herb Potato and Parmesan Quiche\", \"Sourdough Baguette\", \"Herb Potato and Parmesan Quiche Slice\", \"Chocolate Cherry Granola\", \"Chocolate Croissant\", \"Lorraine Quiche\", \"Kouign Amann\", \"Butter Croissant\", \"Olive Ciabatta Loaf\", \"Almond Granola\", \"Lorraine Whole Quiche\", \"Puff Pastry Biscuit\"]",
-                                "objectID": "fb3ff3e72a546_dashboard_generated_id"
+                                vendor_name: 'Manresa Bread',
+                                vendor_slug: 'ba-manresa-bread',
+                                vendor_logo_url:
+                                    'https://uploads-ssl.webflow.com/6226a2abeb906f60cc1f024c/640a594afcee39868b7e1cbf_7fm8EcZgjt4oWPr07HKNqtLX88X-NmQwUYv2InnwogA.png',
+                                vendor_products_array:
+                                    '["Gluten Free Poppy Seed Raspberry Muffin", "Sourdough Bread", "Manresa Almond Croissant", "Monkey Bread", "Herb Potato and Parmesan Quiche", "Sourdough Baguette", "Herb Potato and Parmesan Quiche Slice", "Chocolate Cherry Granola", "Chocolate Croissant", "Lorraine Quiche", "Kouign Amann", "Butter Croissant", "Olive Ciabatta Loaf", "Almond Granola", "Lorraine Whole Quiche", "Puff Pastry Biscuit"]',
+                                objectID:
+                                    'fb3ff3e72a546_dashboard_generated_id',
                             },
                             {
-                                "vendor_name": "La Méditeranée",
-                                "vendor_slug": "ba-la-mediteranee",
-                                "vendor_logo_url": "https://uploads-ssl.webflow.com/6226a2abeb906f60cc1f024c/640a595c3b272e7b102e043e_XO4mRFFVtPyKfqlpsUyjdwJNotzYea99NKLGuD5-pe8.jpeg",
-                                "vendor_products_array": "[\"Hummus Plate\", \"Salmon Kebabs\", \"Tabuleh\", \"Lamb Burger Roll\", \"Saffron Chicken Kebabs (Mary's Free-Range Chicken Breast)\", \"Baba Ghanoush\", \"Pita Bread\", \"Pomegranate Chicken Dinner\", \"Pistachio Nest Dessert\", \"Lebanese Kibbeh Meatballs\", \"Lamb Lule Meatballs Dinner\", \"Djajiki Cucumber Yogurt Dip\", \"Heavenly Baklava Nest\", \"Chicken Cilicia Fillo Pastries\", \"Spinach & Feta Fillo Pastries (Spanikopita)\"]",
-                                "objectID": "320148d61f423_dashboard_generated_id"
+                                vendor_name: 'La Méditeranée',
+                                vendor_slug: 'ba-la-mediteranee',
+                                vendor_logo_url:
+                                    'https://uploads-ssl.webflow.com/6226a2abeb906f60cc1f024c/640a595c3b272e7b102e043e_XO4mRFFVtPyKfqlpsUyjdwJNotzYea99NKLGuD5-pe8.jpeg',
+                                vendor_products_array:
+                                    '["Hummus Plate", "Salmon Kebabs", "Tabuleh", "Lamb Burger Roll", "Saffron Chicken Kebabs (Mary\'s Free-Range Chicken Breast)", "Baba Ghanoush", "Pita Bread", "Pomegranate Chicken Dinner", "Pistachio Nest Dessert", "Lebanese Kibbeh Meatballs", "Lamb Lule Meatballs Dinner", "Djajiki Cucumber Yogurt Dip", "Heavenly Baklava Nest", "Chicken Cilicia Fillo Pastries", "Spinach & Feta Fillo Pastries (Spanikopita)"]',
+                                objectID:
+                                    '320148d61f423_dashboard_generated_id',
                             },
                             {
-                                "vendor_name": "Di Fara Pizza",
-                                "vendor_slug": "ba-di-fara-pizza",
-                                "vendor_logo_url": "https://uploads-ssl.webflow.com/6226a2abeb906f60cc1f024c/6419d5ec7724f400761c5230_c92X0ORC7b8Krgti_YNmaIhiDoMaayySWkmQBDAJtt4.png",
-                                "vendor_products_array": "[\"Sicilian Pizza with Pepperoni\", \"Di Fara Margherita Pizza\", \"Sicilian Pizza\", \"Di Fara's Famous White Pie\", \"New York Style Veggie Pie\", \"New York Style Pepperoni Pizza\"]",
-                                "objectID": "1e1d753ab75d27_dashboard_generated_id"
-                            }
+                                vendor_name: 'Di Fara Pizza',
+                                vendor_slug: 'ba-di-fara-pizza',
+                                vendor_logo_url:
+                                    'https://uploads-ssl.webflow.com/6226a2abeb906f60cc1f024c/6419d5ec7724f400761c5230_c92X0ORC7b8Krgti_YNmaIhiDoMaayySWkmQBDAJtt4.png',
+                                vendor_products_array:
+                                    '["Sicilian Pizza with Pepperoni", "Di Fara Margherita Pizza", "Sicilian Pizza", "Di Fara\'s Famous White Pie", "New York Style Veggie Pie", "New York Style Pepperoni Pizza"]',
+                                objectID:
+                                    '1e1d753ab75d27_dashboard_generated_id',
+                            },
                         ]
                     },
                     getItemUrl({ item }) {
@@ -782,5 +808,13 @@ const { setIsOpen } = autocomplete({
 document.addEventListener('keydown', (event) => {
     if (event.metaKey && event.key.toLowerCase() === 'k') {
         setIsOpen(true)
+    }
+
+    // Disable enter key on search input, keep it enabled for other search selects
+    if (event.key.toLowerCase() === 'enter') {
+        if (event.target.nodeName == 'INPUT' && event.target.type == 'search') {
+            event.preventDefault()
+            return false
+        }
     }
 })
