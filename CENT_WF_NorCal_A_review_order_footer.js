61db6e476fd9f0acd27ce8919da51806c304ab24
Jorge Verdera <110677688+jorgelocale@users.noreply.github.com>
2023-05-31 16:37:32 -0700

    Formatting updates

--- CENT_WF_NorCal_A_review_order_footer.js
+++ CENT_WF_NorCal_A_review_order_footer.js
@@ -20,14 +20,16 @@ function updateCheckoutLinks() {
   );
   console.log("Cart continue:", cartContinueButtons.length, FC.json.item_count);
   if (FC.json.item_count === 1) {
-    cartContinueButtons.forEach(el => {
+    cartContinueButtons.forEach((el) => {
       el.href = `${origin}/bay-area/review-order`;
     });
   }
 }
 
 // Review Page Logic starts here----------
-const reviewItemContainer = document.querySelector(".review-order-item-container");
+const reviewItemContainer = document.querySelector(
+  ".review-order-item-container"
+);
 
 // Funtimes when Foxy's ready.
 var FC = FC || {};
@@ -45,25 +47,28 @@ FC.onLoad = function () {
 // When DOM ready, not when FC.json is loaded
 window.addEventListener("DOMContentLoaded", () => {
   // Add addon to Cart Event Listener
-  document.querySelectorAll(".addon-add-to-cart").forEach(anchor => {
+  document.querySelectorAll(".addon-add-to-cart").forEach((anchor) => {
     anchor.previousSibling.firstChild.getAttribute("href");
-    anchor.addEventListener("click", e => {
-      let addToCartUrlPath = anchor.previousSibling.firstChild.getAttribute("href");
+    anchor.addEventListener("click", (e) => {
+      let addToCartUrlPath =
+        anchor.previousSibling.firstChild.getAttribute("href");
       e.target.innerText = "Adding…";
       addLoadingIcon(reviewItemContainer);
-      FC.client.request(`https://${FC.settings.storedomain}${addToCartUrlPath}`).done(data => {
-        e.target.innerText = "✔ Added";
-        e.target.closest(".addon-item").classList.add("hide");
-        createCartItems();
-      });
+      FC.client
+        .request(`https://${FC.settings.storedomain}${addToCartUrlPath}`)
+        .done((data) => {
+          e.target.innerText = "✔ Added";
+          e.target.closest(".addon-item").classList.add("hide");
+          createCartItems();
+        });
     });
   });
 
   // Animation End event Listener
-  document.querySelectorAll(".addon-item").forEach(addon => {
+  document.querySelectorAll(".addon-item").forEach((addon) => {
     addon.addEventListener(
       "transitionend",
-      ev => {
+      (ev) => {
         if (ev.type === "transitionend") {
           addon.style.display = "none";
         }
@@ -77,10 +82,10 @@ function removeDuplicates() {
   if (!FC.json.items.length) {
     return;
   }
-  document.querySelectorAll(".addon-item-name").forEach(addon => {
+  document.querySelectorAll(".addon-item-name").forEach((addon) => {
     const cartItems = FC.json.items;
 
-    if (cartItems.find(itemInCart => addon.innerText === itemInCart.name)) {
+    if (cartItems.find((itemInCart) => addon.innerText === itemInCart.name)) {
       addon.closest(".addon-item").classList.add("hide");
     }
   });
@@ -106,19 +111,19 @@ function createCartItems() {
 
   // Remove children current cart or loader
   if (reviewItemContainer.children.length) {
-    reviewItemContainer.childNodes.forEach(child => child.remove());
+    reviewItemContainer.childNodes.forEach((child) => child.remove());
   }
 
-  cartItems.forEach(item => {
+  cartItems.forEach((item) => {
     const { id, name, image, price, price_each, quantity, options } = item;
     let vendorName, size, servings;
 
     if (name === "Small Order Fee") return;
     if (options.length) {
-      vendorName = options.find(item => item.name === "Vendor")?.value;
+      vendorName = options.find((item) => item.name === "Vendor")?.value;
       //optional
-      size = options.find(item => item.name === "Size")?.value;
-      servings = options.find(item => item.name === "Servings")?.value;
+      size = options.find((item) => item.name === "Size")?.value;
+      servings = options.find((item) => item.name === "Servings")?.value;
     }
 
     reviewItemContainer.insertAdjacentHTML(
@@ -143,45 +148,62 @@ function createCartItems() {
   updateProgressBar();
 }
 
-function cartItemTemplate(id, name, vendorName, imageURL, price, price_each, quantity) {
+function cartItemTemplate(
+  id,
+  name,
+  vendorName,
+  imageURL,
+  price,
+  price_each,
+  quantity
+) {
   return `<div class="review-cart-item" data-item-id="${id}"><div class="item-image" style="background-image:url(${imageURL})"></div><div class="name-price-checkout"><div class="item-vendor-name"><div>${
     vendorName ? vendorName : ""
   }</div></div><div class="div-block-374"><div class="item-name"><div class="text-block-197">${name}</div></div></div><div class="div-block-513"><div class="div-block-511"><div class="input-quantity-decrement"><div class="html-embed-10 w-embed"><svg id="Layer_1" class="quantity-decrement" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><defs><style>.cls-1{fill:none}.cls-2{fill:currentColor}</style></defs><rect class="cls-2" x="91.12" y="29.94" width="17.76" height="140.11" rx="8.88" ry="8.88" transform="translate(200 0) rotate(90)"></rect><rect class="cls-1" width="200" height="200"></rect></svg></div></div><div class="input-quantity-container">${quantity}</div><div class="input-quantity-increment"><div class="html-embed-10 w-embed"><svg id="Layer_1" class="quantity-increment" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><defs><style>.cls-1{fill:none}.cls-2{fill:currentColor}</style></defs><rect class="cls-2" x="91.12" y="29.94" width="17.76" height="140.11" rx="8.88" ry="8.88"></rect><rect class="cls-2" x="91.12" y="29.94" width="17.76" height="140.11" rx="8.88" ry="8.88" transform="translate(200 0) rotate(90)"></rect><rect class="cls-1" width="200" height="200"></rect></svg></div></div></div></div></div><div class="product-price"><div class="item-price-currency">$</div><div class="item-price">${price_each}</div></div></div>`;
 }
 
 function handleQuantityChange(event) {
-  const handleChange = type => {
+  const handleChange = (type) => {
     const itemContainer = event.target.closest(".review-cart-item");
-    const currentQuantity = itemContainer.querySelector(".input-quantity-container").innerText;
-    const itemFoxyID = event.target.closest("[data-item-id]").getAttribute("data-item-id");
+    const currentQuantity = itemContainer.querySelector(
+      ".input-quantity-container"
+    ).innerText;
+    const itemFoxyID = event.target
+      .closest("[data-item-id]")
+      .getAttribute("data-item-id");
     let changeURL;
     if (type === "decrement")
-      changeURL = `https://${FC.settings.storedomain}/cart?cart=update&quantity=${
+      changeURL = `https://${
+        FC.settings.storedomain
+      }/cart?cart=update&quantity=${
         Number(currentQuantity) - 1
       }&id=${itemFoxyID}`;
 
     if (type === "increment")
-      changeURL = `https://${FC.settings.storedomain}/cart?cart=update&quantity=${
+      changeURL = `https://${
+        FC.settings.storedomain
+      }/cart?cart=update&quantity=${
         Number(currentQuantity) + 1
       }&id=${itemFoxyID}`;
 
     addLoadingIcon(itemContainer);
-    FC.client.request(changeURL).done(json => {
-      const itemToUpdate = json.items.find(item => item.id === itemFoxyID);
+    FC.client.request(changeURL).done((json) => {
+      const itemToUpdate = json.items.find((item) => item.id === itemFoxyID);
       if (!itemToUpdate) {
         itemContainer.remove();
         addSubtotal();
         updateProgressBar();
         return;
       }
-      const { id, name, image, price, price_each, quantity, options } = itemToUpdate;
+      const { id, name, image, price, price_each, quantity, options } =
+        itemToUpdate;
       let vendorName, size, servings;
       if (name === "Small Order Fee") return;
       if (options.length) {
-        vendorName = options.find(item => item.name === "Vendor")?.value;
+        vendorName = options.find((item) => item.name === "Vendor")?.value;
         //optional
-        size = options.find(item => item.name === "Size")?.value;
-        servings = options.find(item => item.name === "Servings")?.value;
+        size = options.find((item) => item.name === "Size")?.value;
+        servings = options.find((item) => item.name === "Servings")?.value;
       }
 
       itemContainer.outerHTML = cartItemTemplate(
@@ -208,5 +230,7 @@ function handleQuantityChange(event) {
 }
 
 function addSubtotal() {
-  document.querySelector(".items-subtotal").innerText = `$${FC.json.total_item_price.toFixed(2)}`;
+  document.querySelector(
+    ".items-subtotal"
+  ).innerText = `$${FC.json.total_item_price.toFixed(2)}`;
 }
